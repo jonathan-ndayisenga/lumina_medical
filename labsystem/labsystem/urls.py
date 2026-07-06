f81ef7dae43c7eecbfe48987f814cb9718ba7a3e
jonathan-ndayisenga <brucendayisenga22@gmail.com>
2026-07-06 11:31:43 +0300

    logo rendering

--- labsystem/labsystem/urls.py
+++ labsystem/labsystem/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,7 +19,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('lab/', include('lab.urls')),  # Lab moved to /lab/ prefix
     path('homecare/', include('homecare.urls')),
+    # Serve uploaded media files (hospital logos etc.) in all environments
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Serve uploaded media files in all environments (hospital logos, etc.)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
